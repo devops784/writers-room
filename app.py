182188a02dc5625f43e7de56eb5ae56beef411b2
muralii <79738694+devops784@users.noreply.github.com>
2026-09-17 16:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🎬 Writers Room</h1>
+    <h2>Story & Screenplay Management Portal</h2>
+    <p>Welcome Murali</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
